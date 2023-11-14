f7f9fb0a70430d90144bebfb12b812098bf63c90
Mahmoud Ibrahim <81623956+mod-ibr@users.noreply.github.com>
2023-11-14 16:21:23 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/agora_one_to_one_video_call/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.agora_one_to_one_video_call
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  AppConstants._();
+
+  static String appId = "da01928a80a74a65ad2496d339c12218";
+  static String token =
+      "007eJxTYBCT/dv+5NblvUnukyvjTv59fui6stqrHRXhtZ8PLAs+Mf2eAkNKooGhpZFFooVBorlJoplpYoqRiaVZirGxZbKhkZGhhVttcGpDICNDwf2PrIwMEAji8zCkZRYVlyRnJOblpeYwMAAAm1wmXA==";
+}
