d44865044b2f4e7f1a68cdc7e58c29f95ba08a5f
Mahmoud Ibrahim <81623956+mod-ibr@users.noreply.github.com>
2023-11-14 16:53:44 +0200

    adding a simple UI

--- lib/home_view/presentation/view/home_view.dart
+++ lib/home_view/presentation/view/home_view.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
-
-class HomeView extends StatelessWidget {
+import 'package:permission_handler/permission_handler.dart';
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+import 'package:agora_rtc_engine/r';
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int? _remoteUid;
+  @override
+  void initState() {
+    super.initState();
+    initForAgora();
+  }
+
+  Future<void> initForAgora() async {}
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Agora One to one Video Call"),
+      ),
+      body: Stack(
+        children: [
+          Center(
+            child: _renderRemoteVideo(),
+          ),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Container(
+              width: 100,
+              height: 100,
+              child: Center(
+                child: _renderLocalePreview(),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _renderLocalePreview() {
+    return Container();
+  }
+
+  Widget _renderRemoteVideo() {
+    if (_remoteUid != null) {
+      return Container();
+    } else {
+      return const Text(
+        "Please wait remote user to join",
+        textAlign: TextAlign.center,
+      );
+    }
   }
 }
